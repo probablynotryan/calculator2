efff571bc91118cb9f05da0aaa6dd0b4406c2619
Ryan Bass <theryanbass@gmail.com>
2022-01-12 11:12:08 -0800

    add subtract, multiply and divide functionality to page with seperate forms
    Co-Authored-by: Alex McKnight <alex.mcknight11@gmail.com>
    Co-Authored-by: Ryan Bass <theryanbass@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
+// Buisness Logic
+
 function add(number1, number2) {
   return number1 + number2;
 }
 
-// function subtract(number1, number2) {
-//   return number1 - number2;
-// }
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
-// function multiply(number1, number2) {
-//   return number1 * number2;
-// }
+function multiply(number1, number2) {
+  return number1 * number2;
+}
 
-// function divide(number1, number2) {
-//   return number1 / number2;
-// }
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// User Interface Logic
 
 $(document).ready(function() {
   $("form#add").submit(function(event){
@@ -20,6 +24,27 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    alert(result);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#subtract1").val());
+    const number2 = parseInt($("#subtract2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#multiply1").val());
+    const number2 = parseInt($("#multiply2").val());
+    const result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#divide1").val());
+    const number2 = parseInt($("#divide2").val());
+    const result = divide(number1, number2);
+    $("#output").text(result);
   });
 });
